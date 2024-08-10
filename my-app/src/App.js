3f45128781c9272c58d3935b5a67fa5aaa38c092
IWillWebDev <willighn469@gmail.com>
2024-08-10 18:39:58 -0300

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
 import Frase from './components/Frase';
+import List from './components/List';
 
 function App() {
   const name = 'Willi'
@@ -11,6 +12,7 @@ function App() {
       <SayMyName nome={name}/>
       <Pessoa nome={name} idade="20" profissao="Engenheiro de Software" foto="https://via.placeholder.com/150"/>
       <Frase/>
+      <List/>
     </div>
   );
 }
