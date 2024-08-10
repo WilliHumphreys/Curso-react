ecf8b9327e5c681bcb8bf372eb2da23d79dda77f
IWillWebDev <willighn469@gmail.com>
2024-08-09 23:44:25 -0300

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 
 function App() {
+  const name = 'Willi'
+
+
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <p>Olá, {name}</p>
     </div>
   );
 }
