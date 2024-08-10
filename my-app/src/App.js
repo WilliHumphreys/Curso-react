4aa48ecee30fbd9a38470992f94f7b15c2dbc4ed
IWillWebDev <willighn469@gmail.com>
2024-08-10 00:07:07 -0300

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
   const name = 'Willi'
+  const url = 'https://via.placeholder.com/150'
 
 
   return (
     <div className="App">
       <h1>Hello World</h1>
       <p>Olá, {name}</p>
+      <img src={url} alt="Imagem de teste"></img>
+      <HelloWorld/>
     </div>
   );
 }
---
+++ my-app/src/components/Frase.js
@@ -0,0 +1,9 @@
+function Frase() {
+    return (
+        <div>
+            <p>Componente com uma frase</p>
+        </div>
+    )
+}
+
+export default Frase
