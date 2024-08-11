ac8ba52143f01d7319a62e23431c88de707e90ff
IWillWebDev <willighn469@gmail.com>
2024-08-11 20:32:08 -0300

    Update Form.js

--- my-app/src/components/Form.js
+++ my-app/src/components/Form.js
@@ -1,16 +1,40 @@
+import { useState } from 'react'
+
 function Form(params) {
 
     function cadUser(e) {
         e.preventDefault()
+        console.log(name)
+        console.log(password)
         console.log("Cadastrou o usuario")
     }
+
+    const [name, setName] = useState()
+    const [password, setPassword] = useState()
 
     return(
         <div>
             <h1>Meu cadastro</h1>
             <form onSubmit={cadUser}>
                 <div>
-                    <input type="text" placeholder="Digite seu nome"/>
+                    <label htmlFor="name">Nome:</label>
+                    <input 
+                        type="text"
+                        id="name"
+                        name="name"
+                        placeholder="Digite seu nome"
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="senha">Senha:</label>
+                    <input 
+                        type="password"
+                        id="senha"
+                        name="senha"
+                        placeholder="Digite sua senha"
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
                 </div>
                 <div>
                     <input type="Submit" value="Cadastrar"/>
