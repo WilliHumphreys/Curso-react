a48aaeeaeb0607b8c0533bd0a52af84be2673d8c
IWillWebDev <willighn469@gmail.com>
2024-08-11 22:17:00 -0300

    update

--- my-app/src/components/layout/Footer.js
+++ my-app/src/components/layout/Footer.js
@@ -1,7 +1,14 @@
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa'
+import styles from './Footer.module.css'
+
 function Footer() {
     return (
         <footer>
-            footer
+            <ul className={styles.socialList}>
+                <li><FaFacebook/></li>
+                <li><FaInstagram/></li>
+                <li><FaLinkedin/></li>
+            </ul>
         </footer>
     )
 }
