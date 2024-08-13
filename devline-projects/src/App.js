6d02d51af60f0382e3b276c7230ecba05b07609c
IWillWebDev <willighn469@gmail.com>
2024-08-12 22:14:24 -0300

    update

--- devline-projects/src/App.js
+++ devline-projects/src/App.js
@@ -1,9 +1,29 @@
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Home from './components/pages/Home'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import NewProject from './components/pages/NewProject'
+
+import Container from './components/layout/Container'
 
 function App() {
   return (
-    <div className="App">
-      <p>Devline Projects</p>
-    </div>
+    <Router>
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/Company">Company</Link>
+        <Link to="/Contact">Contact</Link>
+        <Link to="/NewProject">NewProject</Link>
+      </ul>
+      <Container customClass="minHeight">
+        <Routes> 
+          <Route path='/' element={<Home/>}/>
+          <Route path='/Company' element={<Company/>}/>
+          <Route path='/Contact' element={<Contact/>}/>
+          <Route path='/NewProject' element={<NewProject/>}/>
+        </Routes>
+      </Container>
+    </Router>
   );
 }
 
